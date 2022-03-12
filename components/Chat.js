d856ad94ba80644bf5438156502b712848002ddf
nightorb <ana.nath@gmx.de>
2022-03-12 01:10:21 +0100

    add GiftedChat to chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,22 +1,106 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
+
+// import the Gifted Chat library
+import { Bubble, Day, GiftedChat, SystemMessage } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    // initialize state with an empty array in messages
+    this.state = {
+      messages: []
+    };
+  }
+
+  componentDidMount() {
+    let { name } = this.props.route.params;
+
+    this.setState({
+      // messages must follow a certain format to work with Gifted Chat
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello?',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any'
+          }
+        },
+        {
+          _id: 2,
+          text: `${name} has entered the chat`,
+          createdAt: new Date(),
+          system: true
+        }
+      ]
+    });
+  }
+
+  // function for when user sends a message
+  onSend(messages = []) {
+    // previousState is a reference to the component's state at the time the change is applied
+    this.setState(previousState => ({
+      // the message a user has sent gets appended to the state messages so that it can be displayed in the chat
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
+
+  // function for changing speech bubble color
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#8F3A79'
+          }
+        }}
+      />
+    );
+  }
+
+  // change color of system message
+  renderSystemMessage(props) {
+    return (
+      <SystemMessage
+        {...props}
+        textStyle={{
+          color: '#fff'
+        }}
+      />
+    );
+  }
+
+  // change color of date text
+  renderDay(props) {
+    return (
+      <Day
+        {...props}
+        textStyle={{
+          color: '#fff'
+        }}
+      />
+    );
+  }
+
   render() {
     const { bgColor } = this.props.route.params;
 
     return (
-      <View style={[styles.container, { backgroundColor: bgColor ? bgColor : '#fff' }]}>
-        <Text style={{ color: '#fff'}}>Hello? Yes, this is Dog.</Text>
+      <View style={{ flex: 1, backgroundColor: bgColor ? bgColor : '#fff' }}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          renderSystemMessage={this.renderSystemMessage.bind(this)}
+          renderDay={this.renderDay.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{ _id: 1 }}
+        />
+        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
